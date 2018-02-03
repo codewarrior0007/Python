40369e9b35f1fa1cd2b768e25c1eeb0835d6b26d
cod3breaker666 <cod3breaker666@gmail.com>
2018-02-03 14:07:09 -0600

    Prints user response based on response
    This file shows 3 functions which print user response in upper, lower, and mix faction.

---
+++ functions.py
@@ -0,0 +1,18 @@
+# Playing with functions
+
+#upper() - Prints response in upper case
+feelingtoday = raw_input('How are you doing today?   \n').upper()
+print feelingtoday
+
+
+#lower() - Prints response in lower case
+feelingtoday = raw_input('How are you doing today?   \n').lower()
+print feelingtoday
+
+
+
+#title() - 1st letter of each word is capitalized
+feelingtoday = raw_input('How are you doing today?   \n').title()
+print feelingtoday
+
+
