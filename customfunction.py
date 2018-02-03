63f951cf5569760137287c1a877015de4ff1dbe0
cod3breaker666 <cod3breaker666@gmail.com>
2018-02-03 15:41:16 -0600

    Custom Function Calls
    This file shows how custom function works in Python

---
+++ customfunction.py
@@ -0,0 +1,20 @@
+# Python Custom function & how to call them
+
+def game():
+	print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	print "Print INSIDE the function - I'm in the function"
+	print "==============================================="
+	print "You just won! amazing!".title()
+	print "==============================================="
+	print "Print INSIDE the function - I'm in the function"
+	print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	
+print "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+print "Print BEFORE the function - I'm in the function"
+print "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+game()
+
+print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+print "Print AFTER the function - I'm in the function"
+print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
