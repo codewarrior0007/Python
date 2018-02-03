975e340809b33998d915cfe8973c45b5ec07e256
cod3breaker666 <cod3breaker666@gmail.com>
2018-02-03 14:28:20 -0600

    Adding if else statement
    This file shows how to use if else statement

---
+++ ifelsestatement.py
@@ -0,0 +1,7 @@
+#if else statement
+
+color = raw_input("\Guess my favorite color?\n\n")
+if color == 'green'
+	print 'You got it right! Way to go!! fs'
+else:
+	print 'Nice try, buddy! But my favorite color is yellow!'
