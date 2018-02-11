3b3da941bd72f7f00387b2face57c820fae49f67
cod3breaker666 <cod3breaker666@gmail.com>
2018-02-10 20:21:10 -0600

    Adding Lesson 2
    ADding check for number is zero or not

--- praticepythonDotorglessons/Lesson2-OddOrEvenPart2.py
+++ praticepythonDotorglessons/Lesson2-OddOrEvenPart2.py
@@ -9,16 +9,14 @@
 number = int(raw_input("Please enter a number:      "))
 check = int(raw_input("Please a number to divide by:      "))
 
-if number %  4 == 0 :
-    print(number, "is a multiple of 4")
-elif number %  2 == 0 :
-    print(number, "is an even number")
+if number != 0:
+    if number %  4 == 0 :
+        print(number, "is a multiple of 4")
+    elif number %  2 == 0 :
+        print(number, "is an even number")
+    else:
+        print(number, "is an odd number")
 else:
-    print(number, "is an odd number")
+    print(number, "is zero number")
 
-
-if number % check == 0:
-    print(number, "divides evenly by", check)
-else:
-    print(number, "does not divide evenly by", check)
 
