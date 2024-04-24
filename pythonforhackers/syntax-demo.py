c43ce7b5f43ef9074c3eef07c0976c98f6b362f1
cod3breaker666 <cod3breaker666@gmail.com>
2024-04-23 20:54:09 -0500

    Syntax
    All about syntax

---
+++ pythonforhackers/syntax-demo.py
@@ -0,0 +1,23 @@
+def test():
+	print("test")
+	print("test2")
+
+	string = "string"
+	number = 10
+
+test()
+
+x = 1 + 2 \
+	+ 3 # backslash above breaks the line
+print(x)
+
+
+y = 1 	+ 	1
+print(y)
+
+# Where to go for help?
+# official documentation https:\\docs.python.org\
+# in the terminal - 
+# 	python3 -> help(print) or dir(print)
+# Python style guide
+#	python.org/dev/peps/pep-0008
