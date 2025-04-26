db188de2d4bc350f84fad18933d69c4e08a5aec0
AK <codewarrior0007@users.noreply.github.com>
2025-04-25 22:56:09 -0500

    Adding 1st set of files
    helloworld.py - 1st file created for any new programming languages
     variables.py - Initial file using simple variables.

---
+++ pythonstart/helloworld.py
@@ -0,0 +1,18 @@
+# Hello World 
+
+helloWorld = "Hello world!"
+print(helloWorld)
+
+# This is a comment
+def helloWorld():
+   print("Hello world!")
+# This is a comment 
+
+helloWorld()
+
+
+print("Hellow world!")
+
+
+print("How was your day?")
+print("My day was good!")
---
+++ pythonstart/variables.py
@@ -0,0 +1,63 @@
+#Testing Different Variables
+
+age = 28
+print(age)
+
+
+age2 = 5
+print(age2)
+
+
+# Variables store no, strings, and other types of data
+name = "AA"
+name_height = 5.8
+
+# Shows variable names
+print(f"Name  :", {name})
+print(f"Height  :", {name_height})
+
+# Shows variable values
+print(f"Name  :", name)
+print(f"Height  :",name_height)
+
+
+# Variables help you store values that constantly change
+score = 0
+print(score)
+
+score = 0 + 50 
+print(score)
+
+score = 0 + 100
+print(score)
+
+score = 0 + 300
+print(score)
+
+print(f"The final score was:  {score}")
+
+
+
+
+#Variables allow effecient coding
+
+print(49 / 7)
+
+print(f"SA's age in dog years is {49/7}")
+
+dog_age = 49 / 7
+
+print(f"""SA's age in dog years is {49/7}. So a that's about
+      {49/7} would be the sama ge as SA.  Any dog born about {49/7} 
+years ago would be in the same stage of life as SA""")
+
+print(f"""SA's age in dog years is {dog_age}. So a that's about
+      {dog_age} would be the sama ge as SA.  Any dog born about {dog_age} 
+years ago would be in the same stage of life as SA""")
+
+
+name = "SA"
+
+print(f"""{name}'s age in dog years is {dog_age}. So a that's about
+      {dog_age} would be the sama ge as {name}.  Any dog born about {dog_age} 
+years ago would be in the same stage of life as {name}""")
